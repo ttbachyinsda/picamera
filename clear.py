bc4bc0ffb988bd94918e7e585c09e46078ab1ac5
ttbachyinsda <gfz14@mails.tsinghua.edu.cn>
2017-12-28 22:15:01 +0800

    aasdfsdf

---
+++ clear.py
@@ -0,0 +1,11 @@
+import os
+def clearfiles():
+    path = '/home/pi/new'
+    listfile = os.listdir(path)
+    for element in listfile:
+        print element
+        if 'detect' in element and '.jpg' in element:
+            os.remove(path + '/' + element)
+        else:
+            if 'result' in element and '.txt' in element:
+                os.remove(path + '/' + element)
