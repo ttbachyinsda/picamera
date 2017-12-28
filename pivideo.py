c3e20c5769cf75e4504f54fac258514e1b6b0c51
ttbachyinsda <gfz14@mails.tsinghua.edu.cn>
2017-12-28 22:31:47 +0800

    sdafsda

--- pivideo.py
+++ pivideo.py
@@ -4,7 +4,7 @@ import facetest
 camera = PiCamera()
 camera.resolution=(320,240)
 for i in range(0,10):
-    camera.start_recording('/home/pi/new/video.h264')
+    camera.start_recording('/home/pi/new/video%s.h264' % i)
     time.sleep(2)
     camera.stop_recording()
     camera.stop_preview()
